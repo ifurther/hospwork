cf0820c6fa876efff38e3860006326fd711476b9
ifurther <55025025+ifurther@users.noreply.github.com>
2022-04-09 18:07:02 +0000

    dd

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import (builtins.fetchTarball {
     url = https://github.com/nixos/nixpkgs/archive/a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31.tar.gz;
-    sha256 = "93941a4edb270f434b87b712bb13620a87d6d91f80f70e908a8bf27fd3ec5d58";
+    sha256="162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
   }) {}
 }:
 
