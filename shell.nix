b3ee70a99de20a5b20a827acacd853393ffbfc63
ifurther <55025025+ifurther@users.noreply.github.com>
2022-04-09 21:32:07 +0800

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,33 @@
+{ pkgs ? import (builtins.fetchTarball {
+    url = https://github.com/nixos/nixpkgs/archive/0af613569c9056b2fa1c95b4a99dcb41219bb6dd.tar.gz;
+    sha256 = "07g3mlh0vfbz24v1dqwa0b72wkqky5zjp5yvbmy5cflirz12izzs";
+  }) {}
+}:
+
+
+let
+  r_pkgs = with pkgs.rPackages; [
+    # rmarkdown-related packages.
+    knitr
+    rmarkdown
+    tidyverse
+    viridis
+    # Rstudio-related packages.
+    # servr
+  ];
+in
+
+pkgs.mkShell {
+  name = "dev-shell";
+  buildInputs = [ 
+    pkgs.python38Full
+    pkgs.pipenv
+    pkgs.jupyter
+    pkgs.ipython
+
+    pkgs.pandoc
+    (pkgs.rWrapper.override {
+      packages = r_pkgs;
+    })
+  ];
+}
