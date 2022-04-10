175cf522136174c18075682893306c85c08ac1ad
ifurther <55025025+ifurther@users.noreply.github.com>
2022-04-10 17:54:36 +0800

    add shell.nix

--- shell.nix
+++ shell.nix
@@ -30,15 +30,39 @@ let
     pytest
     rpy2
   ]);
+
+  jupyter = import (builtins.fetchGit {
+    url = https://github.com/tweag/jupyterWith;
+    # Example working revision, check out the latest one.
+    rev = "45f9a774e981d3a3fb6a1e1269e33b4624f9740e";
+  }) {};
+
+  iPython = jupyter.kernels.iPythonWith {
+    name = "python";
+    packages = p: with p; [ numpy ];
+  };
+
+  irkernel = jupyter.kernels.iRWith {                
+      name = "nixpkgs";                                                         
+      # Libraries to be available to the kernel.                                
+      packages = r_pkgs; # p: with p; [ 
+        #ggplot2                                                          
+      #];                                           
+    };
+
+  jupyterEnvironment = (jupyter.jupyterlabWith {                                
+      kernels = [ iPython irkernel ];             
+    }); 
 in
+jupyterEnvironment.env
 pkgs.mkShell {
   name = "dev-shell";
   buildInputs = [ 
     pkgs.pipenv
-    python_pkgs
+    #python_pkgs
 
-    (pkgs.rWrapper.override {
-      packages = r_pkgs;
-    })
+    #(pkgs.rWrapper.override {
+    #  packages = r_pkgs;
+    #})
   ];
 }
