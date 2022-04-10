b2884b24ceeb1ca9c165d1139cb1a5bdadf72633
ifurther <55025025+ifurther@users.noreply.github.com>
2022-04-10 21:53:01 +0800

    add shell.nix

--- shell.nix
+++ shell.nix
@@ -1,28 +1,28 @@
 { pkgs ? import (builtins.fetchTarball {
     url = https://github.com/nixos/nixpkgs/archive/0d95d03c98f2ecef7c721cb6e0a638bbbff44d0a.tar.gz;
-    sha256="162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
+    sha256="1k51s6w8c520w73wmzffvik9kccw4bfccv3xc5hxcm3in1kavgqw";
   }) {}
 }:
 
 let
 
-  jupyterWithPath = import (builtins.fetchGit {
+  jupyter = import (builtins.fetchGit {
     url = https://github.com/tweag/jupyterWith;
     # Example working revision, check out the latest one.
     rev = "45f9a774e981d3a3fb6a1e1269e33b4624f9740e";
   }) {};
 
-  overlays = [
-    (import "${jupyterWithPath}/nix/python-overlay.nix")
-  ];
+  #overlays = [
+  #  (import "${jupyterWithPath}/nix/python-overlay.nix")
+  #];
 
-  pkgs = import <nixpkgs> { inherit overlays; };
-  jupyter = pkgs.jupyterWith;
+  #pkgs = import <nixpkgs> { inherit overlays; };
+  #jupyter = pkgs.jupyterWith;
 
   iPython = jupyter.kernels.iPythonWith {
     name = "python";
-    packages = p: with p; [ pytest pyflakes ];
-    python3 = pkgs.python38Packages;
+    packages = p: with p; [ pytest pyflakes pipenv];
+    python3 = pkgs.python38;
   };
 
   irkernel = jupyter.kernels.iRWith {                
