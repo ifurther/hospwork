93b56c3e47e14a508c50b5464c95fb9e3ba13dee
ifurther <55025025+ifurther@users.noreply.github.com>
2022-04-13 15:36:54 +0800

    add some base functuion for each page

--- cgmh.py
+++ cgmh.py
@@ -5,8 +5,8 @@ import requests
 import pandas as pd
 from urllib.parse import urlparse
 
-url_base='https://www.cgmh.org.tw/tw/Systems/RecruitInfo/'
-url_work_table='3'
+url_base='https://www.cgmh.org.tw/tw/Systems/'
+url_work_table='RecruitInfo/3'
 url=url_base+url_work_table
 g=requests.get(url)
 soup=BeautifulSoup(g.content, 'html.parser')
@@ -25,29 +25,32 @@ def get_pages(pages):
         except:
           counter -= 1
           #print(item,'error')
-      
   return counter
 get_pages(pages)
 
 tables = soup.find('div',class_="bg-grey pd100").find_all('ul')[-2].find_all('li')
 
-def get_work_dead_line( soup):
-    work_detail_web = soup.find('article').get_text().replace("\xa0","").replace('\n',"").replace("\u3000","")
+def get_work_dead_line( soup):  
+  work_detail_web = soup.find('article').get_text().replace("\xa0","").replace('\n',"").replace("\u3000","")
+  if '額滿為止' in work_detail_web:
+    return '額滿為止'
+  elif '即日起收件至' in work_detail_web:
+    dead_line = work_detail_web.rsplit("即日起收件至")[1].split("止")[0].replace("：","")
+    return dead_line      
+  else:
     dead_line = work_detail_web.rsplit("報名期限")[1].split("截止")[0].replace("：","")
     return dead_line
+      
 
-def get_work_page(soup,page):
-    if page == 1:
-        return soup
-    else:
-        g=requests.get(url+'?page='+page)
-        soup=BeautifulSoup(g.content, 'html.parser')
-        return soup
+def get_work_page(page):
+    g=requests.get(url+'?page='+str(page))
+    soup=BeautifulSoup(g.content, 'html.parser')
+    return soup
 
 def get_work_detail(link):
     g=requests.get(link)
     soup=BeautifulSoup(g.content, 'html.parser')
-    return soup  
+    return soup
 
 def get_work_table(soup,tables,work_table):
     for i, item in enumerate(tables):
@@ -62,15 +65,11 @@ def get_work_table(soup,tables,work_table):
             work_table.append([i-2, title, dead_line, work_detail_link ])
 
 work_table=[]
-for i, item in enumerate(tables):
-    if item.find('a'): #過濾掉被刪除的文章
-        s = item.find('a')
-        work_detail_link = url_base+'/'+s.get('href').split('/')[-1]
-        title = item.find_all('div')[1].string
-        dead_line = get_work_dead_line( soup )
-        print('#{}召聘職稱: {} 召聘單位: {}\n 期限: {}\n 連結：{}'.format(i+1, title, origantion, dead_line, work_detail_link ))
-        work_table.append([i-2, title, origantion, dead_line, work_detail_link ])
+for _page in range(1,get_pages(pages)+1):
+    soup_= get_work_page(_page)
+    tables = soup_.find('div',class_="bg-grey pd100").find_all('ul')[-2].find_all('li')
+    get_work_table(soup_,tables,work_table)
 
-work_table=pd.DataFrame(work_table, columns=['no','召聘職稱','召聘單位','期限' ,'連結'])
+work_table=pd.DataFrame(work_table, columns=['no','召聘職稱','期限' ,'連結'])
 
 print(work_table)
