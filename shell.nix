ef4dfa6600fa16ea5c97aa9103fa01b92a1a4c1d
ifurther <55025025+ifurther@users.noreply.github.com>
2022-04-13 18:10:18 +0800

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,48 +1,53 @@
 { pkgs ? import (builtins.fetchTarball {
     url = https://github.com/nixos/nixpkgs/archive/0d95d03c98f2ecef7c721cb6e0a638bbbff44d0a.tar.gz;
-    sha256="1k51s6w8c520w73wmzffvik9kccw4bfccv3xc5hxcm3in1kavgqw";
+    sha256="162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
   }) {}
 }:
-
 let
-
-  jupyter = import (builtins.fetchGit {
-    url = https://github.com/tweag/jupyterWith;
-    # Example working revision, check out the latest one.
-    rev = "45f9a774e981d3a3fb6a1e1269e33b4624f9740e";
-  }) {};
-
-  #overlays = [
-  #  (import "${jupyterWithPath}/nix/python-overlay.nix")
-  #];
-
-  #pkgs = import <nixpkgs> { inherit overlays; };
-  #jupyter = pkgs.jupyterWith;
-
-  iPython = jupyter.kernels.iPythonWith {
-    name = "python";
-    packages = p: with p; [ pytest pyflakes ];
-    python3 = pkgs.python38;
-  };
-
-  irkernel = jupyter.kernels.iRWith {                
-      name = "nixpkgs";                                                         
-      # Libraries to be available to the kernel.                                
-      packages = p: with p; [ 
-        ggplot2
-        dplyr
-        knitr
-        rmarkdown
-        tidyverse
-        viridis
-        tinytex
-      ];                                           
-    };
-
-  jupyterEnvironment = jupyter.jupyterlabWith {                                
-      kernels = [ iPython irkernel ];
-      extraPackages = pp: [pp.pipenv pp.pandoc];
-    }; 
+  r_pkgs = with pkgs.rPackages; [
+    # rmarkdown-related packages.
+    ggplot2
+    dplyr
+    #knitr
+    #rmarkdown
+    #tidyverse
+    #viridis
+    #tinytex
+    # Rstudio-related packages.
+    # servr
+    # Uncomment to add RStudio in your environment.
+    # (pkgs.rstudioWrapper.override {
+    #  packages = r_pkgs;
+    # })
+  ];
+  python_pkgs = pkgs.python38.withPackages (ps: with ps; [
+    pyflakes
+    pytest
+  ]);
 in
+pkgs.mkShell {
+  name = "dev-shell";
+  buildInputs = [ 
+    pkgs.pipenv
+    python_pkgs
+    (pkgs.rWrapper.override {
+      packages = r_pkgs;
+    })
+  ];
+    # Set Environment Variables
+  shellHook = ''
+    SOURCE_DATE_EPOCH=$(date +%s) # required for python wheels
 
-jupyterEnvironment.env
+    local venv=$(pipenv --bare --venv &>> /dev/null)
+
+    if [[ -z $venv || ! -d $venv ]]; then
+      pipenv install --dev &>> /dev/null
+    fi
+
+    export VIRTUAL_ENV=$(pipenv --venv)
+    export PIPENV_ACTIVE=1
+    export PYTHONPATH="$VIRTUAL_ENV/${python3.sitePackages}:$PYTHONPATH"
+    export PATH="$VIRTUAL_ENV/bin:$PATH"
+  '';
+}
+
