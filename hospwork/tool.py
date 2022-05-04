8ed0c7e1da6123f285640eaa8286d50fccdc8e79
ifurther <55025025+ifurther@users.noreply.github.com>
2022-05-04 17:53:52 +0800

    update module tool

--- hospwork/tool.py
+++ hospwork/tool.py
@@ -1,5 +1,14 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
+
+def get_base_web_data(url_base,url_work_table=None):
+    url_base='https://www.cgmh.org.tw/tw/Systems/'
+    url_work_table='RecruitInfo/3'
+    url=url_base+url_work_table
+    g=requests.get(url)
+    soup=BeautifulSoup(g.content, 'html.parser')
+    return soup
+
 
 def get_work_page(page_url,page=None):
     global url
