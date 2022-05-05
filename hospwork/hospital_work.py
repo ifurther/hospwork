59d3c647f59d4f963d11c5f0269bbd5acf5a56e6
ifurther <55025025+ifurther@users.noreply.github.com>
2022-05-05 22:51:34 +0800

    update vghks

--- hospwork/hospital_work.py
+++ hospwork/hospital_work.py
@@ -1,8 +1,5 @@
-class Hospital_work:
+class Hospital_work():
     def __init__(self):
-        self.work_table
-        self.url_base
-        self.url_work
         self.url_full = self.url()
     def url(self):
         return self.url_base+self.url_work
--- hospwork/tool.py
+++ hospwork/tool.py
@@ -2,21 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 
 def get_base_web_data(url_base,url_work_table=None):
-    url_base='https://www.cgmh.org.tw/tw/Systems/'
-    url_work_table='RecruitInfo/3'
-    url=url_base+url_work_table
+    if url_work_table != None:
+        url = url_base + url_work_table
+    else:
+        url = url_base
     g=requests.get(url)
     soup=BeautifulSoup(g.content, 'html.parser')
     return soup
 
 
-def get_work_page(page_url,page=None):
+def get_work_page(page_url,page=None,page_link_part='?page='):
     global url
     if page != None:
-        page_url = page_url+'?page='+str(page)
-    if url == None:
+        page_url = page_url+page_link_part+str(page)
+    if 'url' in globals():
         page_url = url
-        print(page_url)
     g=requests.get(page_url)
     soup=BeautifulSoup(g.content, 'html.parser')
     return soup
