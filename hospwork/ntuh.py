42eec4c346df322006ac8d73a98f77700f8697e3
ifurther <55025025+ifurther@users.noreply.github.com>
2022-05-07 19:42:57 +0800

    update ntuh class

--- hospwork/ntuh.py
+++ hospwork/ntuh.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import json
 from hospwork.hospital_work import Hospital_work
 from hospwork.tool import get_base_web_data,get_work_page
 
@@ -10,27 +11,34 @@ class Ntuh(Hospital_work):
         self.url_base='https://www.ntuh.gov.tw/'
         self.url_work ='ntuh/RecruitAjax!nonDoctor.action'
         self.url_full = super().url()
-        self.work_page_base = get_base_web_data(self.url_full)
-        self.pages_link = self._get_pages_link(self.work_page_base, self.url_full)
+        #self.work_page_base = get_base_web_data(self.url_full)
+        g=requests.get(self.url_full)
+        #soup=BeautifulSoup(g.content, 'html.parser')
+        #tables = soup.findAll('table')[0].findAll('tr')
+        ntu_work_data=json.loads(g.content)
 
+        totalcount = ntu_work_data['totalcount']
+        totalPages = ntu_work_data['totalPages']
         work_table=[]
-for _page in range(1,totalPages):
-    g=requests.get(url+'?page='+str(_page))
-    ntu_work_data=json.loads(g.content)
-    work_table=get_ntuh_work_table_one_page(ntu_work_data,work_table)
+        for _page in range(1,totalPages):
+            g=requests.get(url+'?page='+str(_page))
+            ntu_work_data=json.loads(g.content)
+            work_table=self._get_ntuh_work_table_one_page(ntu_work_data,work_table)
 
-        self.work_table=pd.DataFrame(work_table, columns=['召聘職稱','期限' ,'連結'])
-    def get_ntuh_work_table_one_page(ntu_work_data,work_table):
-    for i, item in enumerate(ntu_work_data['queryList']):
-        title = item['title']
-        origantion = item['jobDepno']
-        begin_date = item['adate_sh']
-        dead_line = item['edatestr']
-        
-        if item['recruitNo']:
-            link = 'https://reg.ntuh.gov.tw/WebApplication/Administration/NtuhGeneralSelect/Entry.aspx?selectno='+item['recruitNo']
-        else:
-            link = url_base+item['ctx'].lstrip('<p><a href="../').split('"><span ')[0]
-        print('#{}召聘職稱: {} 召聘單位: {}\n 期限: {}\n 連結：{}'.format(i+1, title, origantion, dead_line, link))
-        work_table.append([i, title, origantion, dead_line, link])
-    return work_table
+        self.work_table=pd.DataFrame(work_table, columns=['召聘職稱','召聘單位','期限' ,'連結'])
+        print('totalcount: {} getdatacount:{}'.format(totalcount,len(work_table)))
+
+    def _get_ntuh_work_table_one_page(self,ntu_work_data,work_table):
+        for i, item in enumerate(ntu_work_data['queryList']):
+            title = item['title']
+            origantion = item['jobDepno']
+            begin_date = item['adate_sh']
+            dead_line = item['edatestr']
+
+            if item['recruitNo']:
+                link = 'https://reg.ntuh.gov.tw/WebApplication/Administration/NtuhGeneralSelect/Entry.aspx?selectno='+item['recruitNo']
+            else:
+                link = url_base+item['ctx'].lstrip('<p><a href="../').split('"><span ')[0]
+            print('#{}召聘職稱: {} 召聘單位: {}\n 期限: {}\n 連結：{}'.format(i+1, title, origantion, dead_line, link))
+            work_table.append([title, origantion, dead_line, link])
+        return work_table
