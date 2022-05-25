88aa782f4733cabec5dbe16f2a0919d84f3e0e5e
ifurther <55025025+ifurther@users.noreply.github.com>
2022-05-26 03:53:29 +0800

    update cgmh and add test for cgmh

--- hospwork/cgmh.py
+++ hospwork/cgmh.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 from hospwork.hospital_work import Hospital_work
 from hospwork.tool import get_base_web_data,get_work_page
-
+from urllib.parse import urlparse
 
 class Cgmh(Hospital_work):
     def __init__(self):
@@ -11,13 +11,13 @@ class Cgmh(Hospital_work):
         self.url_work = 'RecruitInfo/3'
         self.url_full = super().url()
         self.work_page_base = get_base_web_data(self.url_full)
-        self.pages = self.self.work_page_base.find_all('ul' ,class_="layout__pagination ul-reset")[0]
-    
+        self.pages = self.work_page_base.find_all('ul' ,class_="layout__pagination ul-reset")[0]
+
         work_table=[]
         for _page in range(1,self.get_pages(self.pages)+1):
-            soup_=get_work_page(_page)
+            soup_=get_work_page(self.url_full,page=_page)
             tables = soup_.find('div',class_="bg-grey pd100").find_all('ul')[-2].find_all('li')
-            self.get_work_table(soup_,tables,work_table)
+            self.get_work_table(self.url_full, soup_, tables, work_table)
         self.work_table=pd.DataFrame(work_table, columns=['no','召聘職稱','期限' ,'連結'])
 
     
@@ -34,7 +34,6 @@ class Cgmh(Hospital_work):
                 except:
                     counter -= 1
                     #print(item,'error')
-            
         return counter
 
     def get_work_dead_line(self, soup):  
@@ -55,14 +54,14 @@ class Cgmh(Hospital_work):
             except:
                 return re.findall("\d+年\d+月\d+日",work_detail_web)[0]
     
-    def get_work_table(self,soup,tables,work_table):
+    def get_work_table(self, url_full, soup, tables, work_table):
         for i, item in enumerate(tables):
             if item.find('a'): #過濾掉被刪除的文章
                 s = item.find('a')
-                url_base_website = urlparse(url)
+                url_base_website = urlparse(url_full)
                 work_detail_link = url_base_website._replace(path=urlparse(s.get('href')).path).geturl()
                 title = item.find_all('div')[1].string
-                work_page_soup = get_work_detail(work_detail_link)
-                dead_line = get_work_dead_line( work_page_soup )
+                work_page_soup = get_work_page(work_detail_link)
+                dead_line = self.get_work_dead_line( work_page_soup )
                 print('#{}召聘職稱: {} 期限: {}\n 連結：{}'.format(i+1, title, dead_line, work_detail_link ))
                 work_table.append([i-2, title, dead_line, work_detail_link ])
