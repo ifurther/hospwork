9310fd1a95a73b01d07b590cc690ef9e217c71fa
ifurther <55025025+ifurther@users.noreply.github.com>
2022-06-04 23:22:42 +0800

    remove hch from main ,and add sqlite

--- hospwork/main.py
+++ hospwork/main.py
@@ -1,34 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
 import sys
+from sqlalchemy import create_engine
 from pathlib import Path
 import pandas as pd
+import numpy as np
 
 #sys.path.append(Path().cwd().parent.as_posix())
-from . import Csmpt,Hch,Ylh,Ntuh,Cych,Vghks,Vghtpe
+from . import Csmpt,Ylh,Ntuh,Cych,Vghks,Vghtpe
 
 
-csmpt, hch, ylh, ntuh, cych, vghks, vghtpe = Csmpt(),Hch(),Ylh(),Ntuh(),Cych(),Vghks(),Vghtpe()
-
-
-
-for cc in [csmpt, hch, ylh, ntuh, cych, vghks, vghtpe]:
-    cc.get_full_work_table()
+csmpt, ylh, ntuh, cych, vghks, vghtpe = Csmpt(),Ylh(),Ntuh(),Cych(),Vghks(),Vghtpe()
 
 
 Full_work_table=[]
-for cc in [csmpt, hch, ylh, ntuh, cych, vghks, vghtpe]:
+for cc in [csmpt, ylh, ntuh, cych, vghks, vghtpe]:
+    cc.get_full_work_table()
     Full_work_table.append(cc.get_full_work_table())
 
 
-Full_work_table=pd.concat(Full_work_table)
+Full_work_table=pd.concat(Full_work_table, ignore_index=True).convert_dtypes()
 
 
 Full_work_table[Full_work_table['召聘職稱'].isin(['醫學物理師'])]
 
 
 g=Full_work_table[Full_work_table['召聘職稱'].str.match(r'\S+(醫學物理師|放射師)')==True]
-
-
+print(Full_work_table)
 print(g)
+
+database = r"sqlite:///hospwork.db"
+
+# create a database connection
+db = create_engine(database)
+with db.connect() as conn:
+    #Full_work_table = Full_work_table.fillna(value=np.nan, inplace=True)
+    Full_work_table.to_sql('work_table', conn, if_exists='append', index=False)
+    
+
 
