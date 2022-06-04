66dbef7fe1faa5c5b3d9a4dac2d2054407c1cba6
ifurther <55025025+ifurther@users.noreply.github.com>
2022-06-04 18:15:15 +0800

    update cgmh to full

--- hospwork/cgmh.py
+++ hospwork/cgmh.py
@@ -8,49 +8,54 @@ class Cgmh(Hospital_work):
     def __init__(self):
         self.name = '長庚醫院'
         self.url_base = 'https://www.cgmh.org.tw/tw/Systems/'
-        self.url_work = 'RecruitInfo/3'
+        self.url_work = 'RecruitInfo/3?bulletinType=A&category=Z'
         self.url_full = super().url()
         self.work_page_base = get_base_web_data(self.url_full)
         self.pages = self.work_page_base.find_all('ul' ,class_="layout__pagination ul-reset")[0]
 
         work_table=[]
         for _page in range(1,self.get_pages(self.pages)+1):
-            soup_=get_work_page(self.url_full,page=_page)
+            soup_=get_work_page(self.url_full, page=_page, page_link_part='&page=')
             tables = soup_.find('div',class_="bg-grey pd100").find_all('ul')[-2].find_all('li')
             self.get_work_table(self.url_full, soup_, tables, work_table)
         self.work_table=pd.DataFrame(work_table, columns=['no','召聘職稱','期限' ,'連結'])
 
     
     def get_pages(self, pages):
-        counter=0
-        for i, item in enumerate(pages):
-            if item.find('a') :
-                s = item.find('a')
-                counter += 1
-                try:
-                    #print(s.get("href") ,s.string)
-                    if s.get("href") == "javascript:void(0)":
-                        counter -= 1
-                except:
-                    counter -= 1
-                    #print(item,'error')
-        return counter
+        return int(pages.find_all("li")[-2].text)
 
     def get_work_dead_line(self, soup):  
         work_detail_web = soup.find('article').get_text().replace("\xa0","").replace('\n',"").replace("\u3000","")
-        if '額滿為止' in work_detail_web:
+        if '報名期限展延至' in work_detail_web:
+            return re.findall("\d+年\d+月\d+日",work_detail_web.rsplit('報名期限展延至')[1].split("止")[0].replace(' ',''))[0]
+        elif '額滿' in work_detail_web:
             return '額滿為止'
+        elif '招募合適人選為止' in work_detail_web:
+            return '招募合適人選為止'
         elif '自即日起' in work_detail_web:
             return '自即日起'
         elif '即日起至' in work_detail_web:
-            return re.findall("\d+年\d+月\d+日",work_detail_web.rsplit('即日起至')[1].split("止")[0].replace(' ',''))[0]
+            if "止" in work_detail_web.rsplit('即日起至')[1]:
+                return re.findall("\d+年\d+月\d+日",work_detail_web.rsplit('即日起至')[1].split("止")[0].replace(' ',''))[0]
+            elif "前" in work_detail_web:
+                return re.findall("\d+年\d+月\d+日",work_detail_web.rsplit('即日起至')[1].split("前")[0].replace(' ',''))[0]
+            elif "/" in work_detail_web.rsplit('即日起至')[1]:
+                return re.findall("\d+/\d+/\d+",work_detail_web.rsplit('即日起至')[1].split("。")[0].replace(' ',''))[0]
         elif '即日起收件至' in work_detail_web:
-            return  work_detail_web.rsplit("即日起收件至")[1].split("止")[0].replace("：","")
+            return work_detail_web.rsplit("即日起收件至")[1].split("止")[0].replace("：","")
+        elif '前報名完成' in work_detail_web:
+            return work_detail_web.rsplit("請於")[1].split("前")[0].replace("：","")    
         else:
             try:
-                return work_detail_web.rsplit("報名期限")[1].split("截止")[0].replace("：","")
+                if '截止' in work_detail_web.rsplit("報名期限")[1]:
+                    return work_detail_web.rsplit("報名期限")[1].split("截止")[0].replace("：","")
+                else:
+                    return work_detail_web.rsplit("報名期限")[1].split("。")[0].replace("：","")
             except:
-                return re.findall("\d+年\d+月\d+日",work_detail_web)[0]
+                try:
+                    return re.findall("\d+年\d+月\d+日",work_detail_web)[0]
+                except:
+                    return 'please check webpage'
 
     def get_work_table(self, url_full, soup, tables, work_table):
         for i, item in enumerate(tables):
