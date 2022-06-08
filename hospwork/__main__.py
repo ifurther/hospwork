6567124bb5c6ee1ada9d474e2b8a41da000b7272
ifurther <55025025+ifurther@users.noreply.github.com>
2022-06-08 20:26:27 +0800

    update __main__

--- hospwork/__main__.py
+++ hospwork/__main__.py
@@ -1,13 +1,39 @@
+#!/usr/bin/env python
 
-from .hch import Hch
-from .ylh import Ylh
-
+from sqlalchemy import create_engine
 import pandas as pd
+import numpy as np
+
+#sys.path.append(Path().cwd().parent.as_posix())
+from . import Csmpt,Ylh,Ntuh,Cych,Vghks,Vghtpe
+
+def main():
+    csmpt, ylh, ntuh, cych, vghks, vghtpe = Csmpt(),Ylh(),Ntuh(),Cych(),Vghks(),Vghtpe()
+
+
+    Full_work_table=[]
+    for cc in [csmpt, ylh, ntuh, cych, vghks, vghtpe]:
+        cc.get_full_work_table()
+        Full_work_table.append(cc.get_full_work_table())
+
+
+    Full_work_table=pd.concat(Full_work_table, ignore_index=True).convert_dtypes()
+
+
+    Full_work_table[Full_work_table['召聘職稱'].isin(['醫學物理師'])]
+
+
+    g=Full_work_table[Full_work_table['召聘職稱'].str.match(r'\S+(醫學物理師|放射師)')==True]
+    print(Full_work_table)
+    print(g)
+
+    database = r"sqlite:///hospwork.db"
+
+    # create a database connection
+    db = create_engine(database)
+    with db.connect() as conn:
+        #Full_work_table = Full_work_table.fillna(value=np.nan, inplace=True)
+        Full_work_table.to_sql('work_table', conn, if_exists='append', index=False)
 
 if __name__ == '__main__':
-    hch = Hch()
-    hch.get_full_work_table()
-    ylh = Ylh()
-    ylh.get_full_work_table()
-    print('Call it locally')
-    print(pd.merge(hch.full_work_table,ylh.full_work_table))
+    main()
--- hospwork/main.py
+++ hospwork/main.py
@@ -9,33 +9,33 @@ import numpy as np
 #sys.path.append(Path().cwd().parent.as_posix())
 from . import Csmpt,Ylh,Ntuh,Cych,Vghks,Vghtpe
 
-
-csmpt, ylh, ntuh, cych, vghks, vghtpe = Csmpt(),Ylh(),Ntuh(),Cych(),Vghks(),Vghtpe()
-
-
-Full_work_table=[]
-for cc in [csmpt, ylh, ntuh, cych, vghks, vghtpe]:
-    cc.get_full_work_table()
-    Full_work_table.append(cc.get_full_work_table())
+def main():
+    csmpt, ylh, ntuh, cych, vghks, vghtpe = Csmpt(),Ylh(),Ntuh(),Cych(),Vghks(),Vghtpe()
 
 
-Full_work_table=pd.concat(Full_work_table, ignore_index=True).convert_dtypes()
+    Full_work_table=[]
+    for cc in [csmpt, ylh, ntuh, cych, vghks, vghtpe]:
+        cc.get_full_work_table()
+        Full_work_table.append(cc.get_full_work_table())
 
 
-Full_work_table[Full_work_table['召聘職稱'].isin(['醫學物理師'])]
+    Full_work_table=pd.concat(Full_work_table, ignore_index=True).convert_dtypes()
 
 
-g=Full_work_table[Full_work_table['召聘職稱'].str.match(r'\S+(醫學物理師|放射師)')==True]
-print(Full_work_table)
-print(g)
+    Full_work_table[Full_work_table['召聘職稱'].isin(['醫學物理師'])]
 
-database = r"sqlite:///hospwork.db"
 
-# create a database connection
-db = create_engine(database)
-with db.connect() as conn:
-    #Full_work_table = Full_work_table.fillna(value=np.nan, inplace=True)
-    Full_work_table.to_sql('work_table', conn, if_exists='append', index=False)
+    g=Full_work_table[Full_work_table['召聘職稱'].str.match(r'\S+(醫學物理師|放射師)')==True]
+    print(Full_work_table)
+    print(g)
+
+    database = r"sqlite:///hospwork.db"
+
+    # create a database connection
+    db = create_engine(database)
+    with db.connect() as conn:
+        #Full_work_table = Full_work_table.fillna(value=np.nan, inplace=True)
+        Full_work_table.to_sql('work_table', conn, if_exists='append', index=False)
     
 
 
