3b9aef6f8e8c46b542deb21cd297c7800a33df09
ifurther <55025025+ifurther@users.noreply.github.com>
2022-07-16 00:04:19 +0800

    add ntucc

--- hospwork/__init__.py
+++ hospwork/__init__.py
@@ -4,7 +4,8 @@ from .vghtpe import Vghtpe
 from .hch import Hch
 from .ylh import Ylh
 from .ntuh import Ntuh
+from .ntucc import Ntucc
 from .cych import Cych
 from .tool import get_base_web_data,get_work_page
 
-__all__ = ["__main__", "hospital_work", "tool", "cych", "hch","ntuh", "vhcy"]
+__all__ = ["__main__", "hospital_work", "tool", "cych", "hch","ntuh", "ntucc", "vhcy"]
