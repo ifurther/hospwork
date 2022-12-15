1c2fcb424e2feba137b68b909a340998911e8d8b
ifurther <55025025+ifurther@users.noreply.github.com>
2022-12-15 23:09:35 +0800

    update vghtc

--- test/test_Vghtc.py
+++ test/test_Vghtc.py
@@ -6,9 +6,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from hospwork.vghtc import Vghtc
 
-def test_vghtpe():
+def test_vghtpc():
     vghtc = Vghtc()
     assert vghtc.url_base == 'https://www.vghtc.gov.tw'
     assert isinstance(vghtc.work_table,pd.DataFrame)
     assert len(vghtc.work_table) > 0
-    assert isinstance(vghtc.admit_table,pd.DataFrame)
+    #assert isinstance(vghtc.admit_table,pd.DataFrame)
