c139e87687e3cd7834c6b1cbc7f62f27c9940ca9
ifurther <55025025+ifurther@users.noreply.github.com>
2022-12-15 23:09:16 +0800

    update findjobtype

--- hospwork/tool/job.py
+++ hospwork/tool/job.py
@@ -1,4 +1,4 @@
 import re
 
 def findjobtype(job_type_clean):
-    return re.search(r"[復,營,資,護,家,胸,皮,泌,眼,內,外,藥,麻,臨,急,病,兒,環]\w+[科,部,室,心,課]",job_type_clean).group(0)
+    return re.search(r"[復,營,資,護,家,胸,皮,泌,眼,內,外,藥,麻,臨,急,病,兒,環,健]\w+[科,部,室,心,課]",job_type_clean).group(0)
