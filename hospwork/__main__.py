1ea01c48c268d8efa8271bab019f0d1cac9827fd
ifurther <55025025+ifurther@users.noreply.github.com>
2022-12-24 23:40:31 +0800

    update main

--- hospwork/__main__.py
+++ hospwork/__main__.py
@@ -13,12 +13,16 @@ def main():
 
 
     Full_work_table=[]
+    Full_admit_table=[]
     for cc in [csmpt, ylh, ntuh, cych, vghks, vghtpe, hch, ntucc, vghtc]:
         cc.get_full_work_table()
         Full_work_table.append(cc.get_full_work_table())
+        if len(cc.admit_table) > 0:
+            Full_admit_table.append(cc.get_full_admit_table())
 
 
     Full_work_table=pd.concat(Full_work_table, ignore_index=True).convert_dtypes()
+    Full_admit_table=pd.concat(Full_admit_table, ignore_index=True).convert_dtypes()
 
 
     Full_work_table[Full_work_table['召聘職稱'].isin(['醫學物理師'])]
@@ -29,6 +33,7 @@ def main():
     #print(g)
     print(tabulate(g, headers='keys', tablefmt="pretty")) 
     to_sqlite(Full_work_table)
+    to_sqlite(Full_admit_table,table_name = 'admit_table')
     #g.to_csv('test.csv')
 
 if __name__ == '__main__':
