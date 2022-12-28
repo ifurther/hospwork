8b21906a9433535a6a633393ce702583f2e2c610
ifurther <55025025+ifurther@users.noreply.github.com>
2022-12-28 23:00:44 +0800

    vghtc: update admit table and notebook

--- hospwork/vghtc.py
+++ hospwork/vghtc.py
@@ -10,10 +10,10 @@ class Vghtc(Hospital_work):
         self.local_zone = 'Taiwan'
         self.url_base = 'https://www.vghtc.gov.tw'
         self.url_work = '/Module/RecruitMent?WebMenuID=9d005e46-411b-46fc-b438-e0bd561eba78'
+        self.url_admit = '/Module/Admission?WebMenuID=9d005e46-411b-46fc-b438-e0bd561eba78'
         self.url_full = super().url()
         self.work_page_base = get_base_web_data(self.url_full)
         self.page_links = self.get_page_links(self.url_base, self.url_full, self.work_page_base)
-        self.url_admit = '/Module/Admission?WebMenuID=9d005e46-411b-46fc-b438-e0bd561eba78'
         self.admit_page_base = get_base_web_data(self.url_base+self.url_admit)
         self.admit_tables = self.admit_page_base.find_all('tr')
 
@@ -28,17 +28,20 @@ class Vghtc(Hospital_work):
         work_table.columns=['召聘職稱','召聘單位','詳細連結','期限' ]
         self.work_table=work_table
 
-        # admit_table=[]
-        # for i, item in enumerate(self.admit_tables):
-        #     if item.find('a'): #過濾掉被刪除的文章
-        #         s = item.find('a')
-        #         first_td = item.find('td')
-        #         title = first_td.string
-        #         origantion = first_td.find_next_siblings('td')[0].string
-        #         dead_line = first_td.find_next_siblings('td')[2].string
-        #         #print('#{}召聘職稱: {} 召聘單位: {}\n 期限: {}\n 連結：{}{}'.format(i+1, title, origantion, dead_line, self.url_base, s.get('href')))
-        #         admit_table.append([title, origantion, dead_line, self.url_base+s.get('href')])
-        # self.admit_table=pd.DataFrame(admit_table, columns=['召聘職稱','召聘單位','期限' ,'連結'])
+        admit_table=[]
+        for tt in self.admit_tables:
+            for ttt in tt.findAll('td'):
+                if ttt.get('data-th') == "徵才項目":
+                    title = ttt.text.replace("【錄取公告】","")
+                elif ttt.get('data-th') == '公告迄日':
+                    dead_line = ttt.text
+                elif ttt.get('data-th') == '錄取名單':
+                    job_detail_link = ttt.find('a')
+                    link = self.url_base+job_detail_link.get('href')
+                else:
+                    print("Error",self.name, title)
+            admit_table.append([title, dead_line, link])
+        self.admit_table=pd.DataFrame(admit_table, columns=['召聘職稱','期限' ,'連結'])
 
     def get_page_links(self, url_base, url_full, work_page_base):
         pages_link = []
