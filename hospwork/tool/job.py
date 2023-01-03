755c9c15525ed2dca8a1667d385a8345403b5ac5
ifurther <55025025+ifurther@users.noreply.github.com>
2023-01-04 01:26:45 +0800

    hospwork: add submod job function

--- hospwork/tool/job.py
+++ hospwork/tool/job.py
@@ -1,4 +1,8 @@
 import re
 
-def findjobtype(job_type_clean):
-    return re.search(r"[復,營,資,護,家,胸,皮,泌,眼,內,外,藥,麻,臨,急,病,兒,環,健]\w+[科,部,室,心,課]",job_type_clean).group(0)
+def clean_unused_str(job_text,name)->str:
+    '''clean the unused text'''
+    return job_text.replace('：','').replace("-","").replace('【徵才公告】','').replace("【徵的就是你/妳】","").replace(name,'').replace('【徵才】','')
+def findjobtype(job_type_clean)-> str:
+    '''find the job originzation'''
+    return re.search(r"[放,復,營,資,護,家,胸,皮,泌,眼,內,外,藥,麻,臨,急,病,兒,環,健]\w+[科,部,室,心,課]",job_type_clean).group(0)
