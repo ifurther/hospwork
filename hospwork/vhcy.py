76ad117310a1cb0f3ad25b4a1db84ea766af7c20
ifurther <55025025+ifurther@users.noreply.github.com>
2023-01-04 01:58:23 +0800

    vghcy: update function and fix name

--- hospwork/vhcy.py
+++ hospwork/vhcy.py
@@ -5,22 +5,25 @@ import pandas as pd
 import re
 from hospwork.hospital_work import Hospital_work
 from hospwork.tool.web import get_base_web_data,get_work_page
+from hospwork.tool.job import findjobtype,clean_unused_str
+
 
 class Vhcy(Hospital_work):
     def __init__(self):
-        self.name = '台北榮民總醫院'
+        self.name = '臺中榮民總醫院嘉義分院'
         self.url_base='https://www.vhcy.gov.tw'
         self.url_work = '/PageView/RowView?WebMenuID=1c791b28-2968-49c9-8d5a-32dceca8ad1b'
         self.url_full = super().url()
         self.work_page_base = get_base_web_data(self.url_full)
         self.work_page_work_table = self.work_page_base.find("tbody")
         self.pages_link = self._get_pages_link(self.work_page_base,self.url_base,self.url_full)
-        
-        work_table=[]
-        for p_i, p_item in enumerate(self.pages_link):
-            table_ = get_work_page(p_item)
 
-            work_table = self._get_work_table(self.url_base,table_,work_table)
+        work_table = []
+        exam_table = []
+        admit_table = []
+        for p_item in enumerate(self.pages_link):
+            table_ = get_work_page(p_item)
+            work_table = self._get_work_table(self.url_base,table_,work_table,exam_table,admit_table)
 
         self.work_table=pd.DataFrame(work_table, columns=['召聘職稱','期限' ,"職缺單位" ,'報名簡章'])
 
@@ -43,15 +46,6 @@ class Vhcy(Hospital_work):
             return None
         else:
             return get_work_page(link).find("div",class_="newContent").text.replace("\r","").replace("\t","").split("\n")
-
-
-
-    def get_work_title(self,title):
-        try:
-            new_title = re.search("\B[科,短,社,契,牙,秘,醫,部,室](.*)[員,理,工,師,生]",title).group(0)
-        except AttributeError:
-            print("ERROR:",title)
-        return new_title
 
 
 
@@ -97,15 +91,20 @@ class Vhcy(Hospital_work):
                 dead = "check page"
             return dead
 
-    def _get_work_table(self,url_base,table_,work_table):
+    def _get_work_table(self,url_base,table_,work_table,exam_table,admit_table):
         for p_i, p_item in enumerate(table_.find_all("tr")):
             if p_item.find('a'):
                 p_item_a = p_item.find('a')
                 title = p_item_a.text
-                if "錄取公告" in title or '考試公告' in title or "甄試結果公告" in title or "核定" in title:
-                    break
-                new_title = self.get_work_title(title)
                 link_s = url_base+p_item_a.get('href')
+                if "錄取公告" in title or "甄試結果公告" in title or "核定" in title:
+                    admit_table.append(title.replace("錄取公告：",""), link_s)
+                elif '考試公告' in title:
+                    exam_table.append(title.replace("考試公告：",""), link_s)
+                else:
+                    pass
+                new_title = findjobtype(title)
+
                 if self.get_work_detail(link_s) == None:
                     break
                 origination = self.get_work_originazation(title,link_s)
