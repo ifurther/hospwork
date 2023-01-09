0aa9fb02c24981bdb1f24bb6e22ec22b63a57588
ifurther <55025025+ifurther@users.noreply.github.com>
2023-01-09 23:59:55 +0800

    ntuh: update get work table

--- hospwork/ntuh.py
+++ hospwork/ntuh.py
@@ -9,15 +9,16 @@ from hospwork.tool.web import get_base_web_data,get_work_page
 class Ntuh(Hospital_work):
     def __init__(self):
         self.name = '國立臺灣大學醫學院附設醫院'
-        self.url_base='https://www.ntuh.gov.tw/'
-        self.url_work ='ntuh/RecruitAjax!nonDoctor.action'
+        self.url_base='https://www.ntuh.gov.tw/ntuh'
+        self.url_work ='/RecruitAjax!nonDoctor.action'
+        self.url_admit = '/RecruitAjax!select.action'
         self.url_full = super().url()
         #self.work_page_base = get_base_web_data(self.url_full)
         g=requests.get(self.url_full)
         #soup=BeautifulSoup(g.content, 'html.parser')
         #tables = soup.findAll('table')[0].findAll('tr')
         ntu_work_data=json.loads(g.content)
-
+        self.admit_table = []
         totalcount = ntu_work_data['totalcount']
         totalPages = ntu_work_data['totalPages']
         work_table=[]
@@ -36,10 +37,12 @@ class Ntuh(Hospital_work):
             begin_date = item['adate_sh']
             dead_line = item['edatestr']
 
-            if item['recruitNo']:
+            if item['recruitNo'] and "分院" not in origantion:
                 #link = 'https://reg.ntuh.gov.tw/WebApplication/Administration/NtuhGeneralSelect/Entry.aspx?selectno='+item['recruitNo']
                 detail_link = 'https://reg.ntuh.gov.tw/NtuhGeneralSelect/Openpdf.aspx?SelectNo='+item['recruitNo']+'&FileName='+item['recruitNo']+'.pdf'
                 resume_link = 'https://reg.ntuh.gov.tw/NtuhGeneralSelect/Entry.aspx?selectno='+item['recruitNo']
+            elif "分院" in origantion:
+                break
             else:
                 detail_link = url_base+item['ctx'].lstrip('<p><a href="../').split('"><span ')[0]
                 resume_link = ''
