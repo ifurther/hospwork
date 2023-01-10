91d3889858dbf50707dd9eb379c65d80a789dd3a
ifurther <55025025+ifurther@users.noreply.github.com>
2023-01-10 17:17:50 +0800

    tool: update some function

--- hospwork/tool/job.py
+++ hospwork/tool/job.py
@@ -6,14 +6,14 @@ def clean_unused_str(job_text,name)->str:
 def findjobtype(job_type)-> str:
     '''find the job type'''
     try:
-        return re.search(r"\B[科,短,社,契,牙,秘,醫,部,室,門,招,募](.*)[員,理,工,師,生]",job_type).group(0).replace('募','')
+        return re.search(r"\B[科,短,社,契,牙,秘,醫,部,室,門,招,募](.*)[員,理,工,師,生]",job_type).group(0).replace("招募","").replace('募','').replace("聘","")
     except AttributeError:
-        print("Error findjobtype:",job_type)
+        print("Error find job type:",job_type)
         return job_type
 def findjoboriginzation(job_type):
     '''find the job originzation'''
     try:
-        return re.search(r"[教,影,癌,血,牙,腦,骨,巨,工,傳,耳,放,復,營,資,護,家,胸,皮,泌,眼,內,外,藥,麻,臨,急,病,兒,環,健](\w+||)[科,部,室,心,課,處]",job_type).group(0)
+        return re.search(r"([婦,神,5,管,新,教,影,癌,血,牙,腦,骨,巨,工,傳,耳,放,復,營,資,護,家,胸,皮,泌,眼,外,藥,麻,臨,急,病,兒,環,健](\w+||)[科,部,室,心,課,處,房,局])|([內,中]\w+[科,局])",job_type).group(0)
     except AttributeError:
-        print("Error findjobtype: {}".format(job_type))
+        print("Error find originzation: {}".format(job_type))
         return job_type
