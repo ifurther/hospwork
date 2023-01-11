7477606bc084b44c399e6410655a6535859d6d63
ifurther <55025025+ifurther@users.noreply.github.com>
2023-01-11 23:41:37 +0800

    hospwork: update pytest file

--- test/test_Ntuh.py
+++ test/test_Ntuh.py
@@ -11,5 +11,6 @@ def test_ntuh():
     assert ntuh.url_base == 'https://www.ntuh.gov.tw/ntuh'
     assert isinstance(ntuh.work_table,pd.DataFrame)
     assert isinstance(ntuh.admit_table,pd.DataFrame)
-
+    assert not ntuh.work_table.empty, "the work_table is non empty"
+    assert not ntuh.admit_table.empty, "the admit_table is non empty"
     
--- test/test_Vhcy.py
+++ test/test_Vhcy.py
@@ -9,3 +9,8 @@ from hospwork.vhcy import Vhcy
 def test_vhcy():
     vhcy = Vhcy()
     assert isinstance(vhcy.work_table,pd.DataFrame)
+    assert isinstance(vhcy.exam_table,pd.DataFrame)
+    assert isinstance(vhcy.admit_table,pd.DataFrame)
+    assert not vhcy.work_table.empty, "work_table is null"
+    assert not vhcy.exam_table.empty, "exam_table is null"
+    assert not vhcy.admit_table.empty, "admit_table is null"
