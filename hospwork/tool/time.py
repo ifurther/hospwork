515f3587e5f53dc7f7f9bf70b3923f01d2fb6024
ifurther <55025025+ifurther@users.noreply.github.com>
2023-01-18 01:31:09 +0800

    hospwork: add clean date function

---
+++ hospwork/tool/time.py
@@ -0,0 +1,16 @@
+import re,datetime
+
+def clean_date(time_raw):
+    '''tansfer date'''
+
+    if (roc_date:= re.sub('[年,月]','-',time_raw.replace('日','')) ) and roc_date != time_raw and (roc_year := roc_date.split('-')[0]+1911) and abs(roc_year - datetime.date.today().year) <= 3:
+        time_new = time_raw.replace(roc_date.split('-')[0],roc_year)
+    elif '/' in time_raw:
+        time_new = time_raw.replace('/','-')
+    else:
+        time_new = time_raw
+    if (time_raw_checked := re.search('\d+[-,/,年]\d{1,2}[-,/,月]\d{1,2}',time_new) ):
+        return datetime.datetime.strptime(time_raw_checked.group(0),"%Y-%m-%d").date()
+    else:
+        print('Error:',time_raw)
+        return time_raw
