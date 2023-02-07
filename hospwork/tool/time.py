e57b44efbb950ef6738e01ae9d4a59671c4ca9f6
ifurther <55025025+ifurther@users.noreply.github.com>
2023-02-08 00:45:00 +0800

    hospwork.tool: update find time function

--- hospwork/tool/time.py
+++ hospwork/tool/time.py
@@ -1,6 +1,6 @@
 import re,datetime
 
-def clean_date(time_raw):
+def clean_date(time_raw,dtype='str'):
     '''tansfer date'''
 
     if (roc_date:= re.sub('[年,月]','-',time_raw.replace('日','')) ) and roc_date != time_raw and (roc_year := roc_date.split('-')[0]+1911) and abs(roc_year - datetime.date.today().year) <= 3:
@@ -9,8 +9,10 @@ def clean_date(time_raw):
         time_new = time_raw.replace('/','-')
     else:
         time_new = time_raw
-    if (time_raw_checked := re.search('\d+[-,/,年]\d{1,2}[-,/,月]\d{1,2}',time_new) ):
+    if (time_raw_checked := re.search('\d+[-,/,年]\d{1,2}[-,/,月]\d{1,2}',time_new) ) and dtype == 'datetime':
         return datetime.datetime.strptime(time_raw_checked.group(0),"%Y-%m-%d").date()
+    elif  (time_raw_checked := re.search('\d+[-,/,年]\d{1,2}[-,/,月]\d{1,2}',time_new) )  and dtype == 'str':
+        return str(datetime.datetime.strptime(time_raw_checked.group(0),"%Y-%m-%d").date())
     else:
         print('Error:',time_raw)
         return time_raw
