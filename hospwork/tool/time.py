8ccbe0dcff45df0c1d0a768732e453e396c6c56c
ifurther <55025025+ifurther@users.noreply.github.com>
2023-02-08 02:50:38 +0800

    hospwork.tool: update find time function

--- hospwork/tool/time.py
+++ hospwork/tool/time.py
@@ -1,18 +1,29 @@
 import re,datetime
 
-def clean_date(time_raw,dtype='str'):
-    '''tansfer date'''
-
-    if (roc_date:= re.sub('[年,月]','-',time_raw.replace('日','')) ) and roc_date != time_raw and (roc_year := roc_date.split('-')[0]+1911) and abs(roc_year - datetime.date.today().year) <= 3:
-        time_new = time_raw.replace(roc_date.split('-')[0],roc_year)
-    elif '/' in time_raw:
-        time_new = time_raw.replace('/','-')
+def transfer_year(time_raw):
+    '''transfer year from roc to ac'''
+    roc_date = time_raw
+    if (roc_year := int(roc_date.split('-')[0]) ) and roc_year < 1911 and abs( (roc_year + 1911) - datetime.date.today().year) <= 3:
+        time_new = roc_date.replace(roc_date.split('-')[0],str(roc_year + 1911))
     else:
-        time_new = time_raw
+        time_new = roc_date   
+    return time_new
+ 
+def clean_date(time_raw,hosp_name,dtype='str'):
+    '''tansfer date'''
+    if (time_:= re.sub('[年,月]','-',time_raw.replace('日','')) ) and time_ != time_raw:
+        time_new = transfer_year(time_)
+    elif '/' in time_raw:
+        if len(time_raw.split('/')) < 3:
+            return time_raw
+        else:
+            time_new = transfer_year(time_raw.replace('/','-'))
+    else:
+        time_new =  transfer_year(time_raw)
     if (time_raw_checked := re.search('\d+[-,/,年]\d{1,2}[-,/,月]\d{1,2}',time_new) ) and dtype == 'datetime':
         return datetime.datetime.strptime(time_raw_checked.group(0),"%Y-%m-%d").date()
-    elif  (time_raw_checked := re.search('\d+[-,/,年]\d{1,2}[-,/,月]\d{1,2}',time_new) )  and dtype == 'str':
+    elif (time_raw_checked := re.search('\d+[-,/,年]\d{1,2}[-,/,月]\d{1,2}',time_new) )  and dtype == 'str':
         return str(datetime.datetime.strptime(time_raw_checked.group(0),"%Y-%m-%d").date())
     else:
-        print('Error:',time_raw)
+        print(hosp_name,'Error clear time data and time raw data is :',time_raw)
         return time_raw
