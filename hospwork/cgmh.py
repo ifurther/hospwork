666d64340a937c72bff01aae634b49ed7146e491
ifurther <55025025+ifurther@users.noreply.github.com>
2023-02-08 00:36:57 +0800

    cgmh: update work find deadline

--- hospwork/cgmh.py
+++ hospwork/cgmh.py
@@ -3,6 +3,7 @@ import re
 from hospwork.hospital_work import Hospital_work
 from hospwork.tool.web import get_base_web_data,get_work_page
 from hospwork.tool.job import findjoboriginzation,findjobtype,clean_unused_str
+from hospwork.tool.time import clean_date
 from hospwork.error import errormsg
 from urllib.parse import urlparse
 
@@ -22,19 +23,22 @@ class Cgmh(Hospital_work):
         self.work_page_base_admit = get_base_web_data(self.url_base+self.url_admit)
         self._pages_admit = self.work_page_base_work.find_all('ul' ,class_="layout__pagination ul-reset")[0]
 
-        work_table=[]
-        admit_table=[]
+        work_table = []
+        exam_table = []
+        admit_table = []
         for _page in range(1,self.get_pages(self._pages_work)+1):
             soup_=get_work_page(self.url_full, page=_page, page_link_part='&page=')
             tables = soup_.find('div',class_="bg-grey pd100").find_all('ul')[-2].find_all('li')
-            self.get_work_table(self.url_full, soup_, tables, work_table)
-        self.work_table=pd.DataFrame(work_table, columns=['no','召聘職稱', "召聘單位", '院區', '期限', '詳細連結', '報名連結'])
+            self.get_work_table(self.url_full, soup_, tables, work_table, exam_table, admit_table)
+        self.work_table=pd.DataFrame(work_table, columns=['召聘職稱', "召聘單位", '院區', '期限', '詳細連結', '報名方式','報名連結'])
+        self.exam_table=pd.DataFrame(exam_table)
         self.admit_table=pd.DataFrame(admit_table)
 
     def get_pages(self, pages):
         return int(pages.find_all("li")[-2].text)
 
     def get_work_dead_line(self, work_detail_web, title):
+        work_detail_web = work_detail_web.replace('(日) ','')
         if '報名期限展延至' in work_detail_web:
             return re.findall("\d+年\d+月\d+日",work_detail_web.rsplit('報名期限展延至')[1].split("止")[0].replace(' ',''))[0]
         elif '額滿' in work_detail_web:
@@ -47,7 +51,8 @@ class Cgmh(Hospital_work):
             return '即日起至招聘完成'
         elif '自即日起' in work_detail_web or '即日起~' in work_detail_web:
             return '自即日起'
-        elif '即日起至' in work_detail_web:
+        elif '即日起至' in work_detail_web or '即日起，至' in work_detail_web:
+            work_detail_web = work_detail_web.replace('即日起，至','即日起至')
             if "止" in work_detail_web.rsplit('即日起至')[1]:
                 return re.findall("\d+年\d+月\d+日",work_detail_web.rsplit('即日起至')[1].split("止")[0].replace(' ',''))[0]
             elif "前" in work_detail_web:
@@ -95,23 +100,24 @@ class Cgmh(Hospital_work):
         elif '北院區' in title:
             return '北院區'
 
-    def get_work_table(self, url_full, soup, tables, work_table):
+    def get_work_table(self, url_full, soup, tables, work_table, exam_table, admit_table):
         for i, item in enumerate(tables):
-            if item.find('a'): #過濾掉被刪除的文章
+            if item.find('a'):
                 s = item.find('a')
                 url_base_website = urlparse(url_full)
                 work_detail_link = url_base_website._replace(path=urlparse(s.get('href')).path).geturl()
                 title = clean_unused_str(item.find_all('div')[1].string,self.name)
+                title_old = title
                 work_page_soup = get_work_page(work_detail_link)
                 try:
                     work_detail_web = work_page_soup.find('article').get_text().replace("\xa0","").replace('\n',"").replace("\u3000","")
                 except:
                     print(work_page_soup)
                     raise AttributeError
-                if '長庚大學' in title or '甄試結果公告' in title:
+                if '長庚大學' in title:
                     break
                 if (new_title :=  findjobtype(title, self.name)) and new_title != title:
-                    title_old = title
+
                     title = new_title.replace('醫院','').replace("紀念",'').replace("新北市立","").replace("高雄市立鳳山","")
                 if (region := self.get_hosp_region((title_ if (title_ := title_old) else title))) and region is not None:
                     region = region
@@ -119,12 +125,32 @@ class Cgmh(Hospital_work):
                 if (originzation := findjoboriginzation((title_ if (title_ :=  title_old) else title), self.name)) and originzation != title:
                     originzation = originzation
                     title = title.replace(originzation,'')
-                elif (originzation := findjoboriginzation(work_detail_web) ):
+                elif (originzation := findjoboriginzation(work_detail_web, self.name) ):
                     originzation = originzation
                     title = title.replace(originzation,'')
                 else:
                     originzation = ''
                 dead_line = self.get_work_dead_line( work_detail_web ,title)
-                resume_link = 'https://webapp.cgmh.org.tw/resume/adm.ASP'
+                if '線上登錄履歷' in work_detail_web:
+                    resume_link = 'https://webapp.cgmh.org.tw/resume/adm.ASP'
+                    apply_type = 'online'
+                elif 'E-mail' in work_detail_web or re.search('\w+@\w+.\w+.\w+',work_detail_web):
+                    resume_link = ''
+                    apply_type = 'email'
+                elif '書面報名' in work_detail_web:
+                    resume_link = ''
+                    apply_type = 'mail'                
+                else:
+                    pass
                 #print('#{}召聘職稱: {} 期限: {}\n 連結：{}'.format(i+1, title, dead_line, work_detail_link ))
-                work_table.append([i-2, title, originzation, region, dead_line, work_detail_link, resume_link ])
+
+                if '口試' in title or '筆試' in title:
+                    exam_table.append([title, originzation, region, dead_line, work_detail_link])
+                elif '甄試結果' in title:
+                    admit_table.append([title, originzation, region, work_detail_link])
+                else:
+                    if apply_type is not None:
+                        work_table.append([title, originzation, region, dead_line, work_detail_link, apply_type, resume_link ])
+
+                    else:
+                        print(self.name,'Error find resume:',title,work_detail_link)
