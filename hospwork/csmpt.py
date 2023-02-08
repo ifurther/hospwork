301bdcf5eecfdb6814f726efcbc375ee2809d3f5
ifurther <55025025+ifurther@users.noreply.github.com>
2023-02-08 18:24:32 +0800

    csmpt: add tool new function

--- hospwork/csmpt.py
+++ hospwork/csmpt.py
@@ -2,7 +2,8 @@ import pandas as pd
 import re
 from hospwork.hospital_work import Hospital_work
 from hospwork.tool.web import get_base_web_data,get_work_page
-
+from hospwork.tool.job import clean_unused_str
+from hospwork.tool.time import clean_date
 
 class Csmpt(Hospital_work):
     def __init__(self):
@@ -36,15 +37,15 @@ class Csmpt(Hospital_work):
         return pages_link
 
     def _get_work_deadtime(link):
-        y=get_work_detail(link).find('div', class_="content").text.replace('\xa0','').replace("\n","").replace(" 年","年").replace(" 月","月").replace(" 日","日")
+        work_detail_page_data = clean_unused_str(get_work_detail(link).find('div', class_="content").text, self.name)
         date_pattern="(\d{1,4}年)((([0?][1-9])月)|(([1?][0-2])月)|([1-9]月)?)(([0?][1-9]日)|([1?][0-9]日)|([2?][1-9]日)|([3][0-1]日)?)"
-        return re.search(date_pattern,re.search("(?<=期限)(.*)年(.*)月(.*)日",y)[0]).group(0)
+        return clean_date(re.search(date_pattern,re.search("(?<=[期限,截止])(.*)年(.*)月(.*)日",work_detail_page_data)[0]).group(0), self.name)
 
     def _get_work_table(self,table_,url_base,work_table):
         for p_i, p_item in enumerate(table_.find_all('li')):
             if p_item.find('a'):
                 p_item_a = p_item.find('a')
-                title = p_item_a.text
+                title = clean_unused_str(p_item_a.text.replace(' ',''), self.name)
                 link_s = url_base+'/'+p_item_a.get('href')
                 try:
                     dead_line = _get_work_deadtime(link_s)
