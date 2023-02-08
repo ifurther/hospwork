fe8075e134bb3ccd9497a7e17834394ee81d14ec
ifurther <55025025+ifurther@users.noreply.github.com>
2023-02-08 23:30:17 +0800

    hospwork.io: update sqlite function

--- hospwork/io/sqlite.py
+++ hospwork/io/sqlite.py
@@ -8,13 +8,11 @@ import pandas as pd
 import numpy as np
 
 
-def to_sqlite(Full_work_table,table_name = 'work_table', database = r"sqlite:///hospwork.db"):
+def to_sqlite(Full_work_table,table_name = 'work_table', database = "sqlite:///hospwork.db"):
 
     # create a database connection
     db = create_engine(database)
-    with db.connect() as conn:
-        #Full_work_table = Full_work_table.fillna(value=np.nan, inplace=True)
-        Full_work_table.to_sql(table_name, conn, if_exists='replace', index=False, dtype={'name_of_json_column_in_source_table': JSON})
+    Full_work_table.to_sql(table_name, db, if_exists='replace', index=False, dtype={'name_of_json_column_in_source_table': JSON})
     print('success')
 
 
