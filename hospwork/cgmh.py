22403707c436dcd8af0de05c20a37691852c5af9
ifurther <55025025+ifurther@users.noreply.github.com>
2023-02-10 22:47:06 +0800

    cgmh: update get full admit

--- hospwork/cgmh.py
+++ hospwork/cgmh.py
@@ -38,7 +38,7 @@ class Cgmh(Hospital_work):
             self.get_work_table(self.url_full, soup_, tables, work_table, exam_table, admit_table)
         self.work_table=pd.DataFrame(work_table, columns=['召聘職稱', "召聘單位", '院區', '期限', '詳細連結', '報名方式','報名連結'])
         self.exam_table=pd.DataFrame(exam_table, columns=['召聘職稱', "召聘單位", '院區', '期限', '詳細連結'])
-        self.admit_table=pd.DataFrame(admit_table, columns=['召聘職稱', "召聘單位", '院區', '詳細連結'])
+        self.admit_table=pd.DataFrame(admit_table, columns=['召聘職稱', "召聘單位", '院區', '連結'])
 
     def get_pages(self, pages):
         return int(pages.find_all("li")[-2].text)
