114332bbc86d31cc1f6389414179c4bff77714b2
ifurther <55025025+ifurther@users.noreply.github.com>
2023-02-13 18:00:28 +0800

    ntuh: update get admit function

--- hospwork/ntuh.py
+++ hospwork/ntuh.py
@@ -29,11 +29,12 @@ class Ntuh(Hospital_work):
             g=requests.get(self.url_full+'?page='+str(_page))
             ntu_work_data=json.loads(g.content)
             self._get_ntuh_work_table_one_page(self.url_base,ntu_work_data,work_table)
+        exam_table = []
         admit_table = []
         for _page in range(1,totalPages_admit):
             g=requests.get(self.url_full_admit+'?page='+str(_page))
             ntu_admit_data=json.loads(g.content)
-            self._get_ntuh_work_table_one_page(self.url_base,ntu_admit_data,admit_table)
+            self._get_ntuh_admit_table_one_page(self.url_base, ntu_admit_data, exam_table, admit_table)
 
         self.work_table = pd.DataFrame(work_table, columns=['召聘職稱','召聘單位','期限' ,'詳細連結','報名連結'])
         self.admit_table = pd.DataFrame(admit_table, columns=['召聘職稱','召聘單位','期限' ,'連結','詳細連結']) 
@@ -60,3 +61,24 @@ class Ntuh(Hospital_work):
                 resume_link = ''
             #print('#{}召聘職稱: {} 召聘單位: {}\n 期限: {}\n 連結：{}'.format(i+1, title, origantion, dead_line, link))
             work_table.append([title, origantion, dead_line, detail_link, resume_link])
+    def _get_ntuh_admit_table_one_page(self, url_base, ntu_admit_data, exam_table, admit_table):
+        for i, item in enumerate(ntu_admit_data['queryList']):
+            title = clean_unused_str(item['title'],self.name)
+            origantion = item['jobDepno']
+            begin_date = item['adate_sh']
+            if item['edatestr'] is not '':
+                dead_line = clean_date(item['edatestr'].replace('至',''), self.name)
+            else:
+                dead_line = 'please check page'
+
+            if item['recruitNo'] and "分院" not in origantion:
+                #link = 'https://reg.ntuh.gov.tw/WebApplication/Administration/NtuhGeneralSelect/Entry.aspx?selectno='+item['recruitNo']
+                detail_link = 'https://reg.ntuh.gov.tw/NtuhGeneralSelect/Openpdf.aspx?SelectNo='+item['recruitNo']+'&FileName='+item['recruitNo']+'.pdf'
+                resume_link = 'https://reg.ntuh.gov.tw/NtuhGeneralSelect/Entry.aspx?selectno='+item['recruitNo']
+            elif "分院" in origantion:
+                break
+            else:
+                detail_link = url_base+item['ctx'].lstrip('<p><a href="../').split('"><span ')[0]
+                resume_link = ''
+            #print('#{}召聘職稱: {} 召聘單位: {}\n 期限: {}\n 連結：{}'.format(i+1, title, origantion, dead_line, link))
+            admit_table.append([title, origantion, dead_line, detail_link, resume_link])
