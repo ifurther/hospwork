af1ed6af41c631a059dc3552c61d9f0a0de3e94f
ifurther <55025025+ifurther@users.noreply.github.com>
2024-07-20 02:40:58 +0800

    vghtpe: add notebook for test

--- hospwork/vghtpe.py
+++ hospwork/vghtpe.py
@@ -12,20 +12,18 @@ class Vghtpe(Hospital_work):
         self.url_full = super().url()
         self.url_admit = '/conscribe/indexa.htm?openExternalBrowser=1'
 
-        User_Agent = {
+        headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.78',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
             'Connection': 'keep-alive',
             "Cache-Control": "no-cache",
             "Priority": "u=4",
-            "Referer": "https://www.vghtpe.gov.tw/",
-            "Upgrade-Insecure-Requests": "1",
         }
 
-        self.work_page_base = get_base_web_data(self.url_full, headers=User_Agent)
+        self.work_page_base = get_base_web_data(self.url_full, headers=headers)
         self.work_tables = self.work_page_base.find_all('tr')
 
-        self.admit_page_base = get_base_web_data(self.url_base+self.url_admit)
+        self.admit_page_base = get_base_web_data(self.url_base+self.url_admit, headers = User_Agent)
         self.admit_tables = self.admit_page_base.find_all('tr')
 
         work_table=[]
