b6626cf832cf490a87b7007b35cf8dd22e22f722
ifurther <55025025+ifurther@users.noreply.github.com>
2025-02-16 23:38:19 +0800

    scrapy.cgumh: add find dead function

--- hospwork/hospwork/spiders/cgumh.py
+++ hospwork/hospwork/spiders/cgumh.py
@@ -1,5 +1,10 @@
-import scrapy
+import scrapy,re
 from urllib.parse import urlparse,urlencode,parse_qsl
+
+from hospwork.items import HospworkItem
+from hospwork.tool.web import get_base_web_data,get_work_page
+from hospwork.tool.job import findjoboriginzation,findjobtype,clean_unused_str
+from hospwork.tool.time import clean_date
 
 class CgumhSpider(scrapy.Spider):
     name = "cgumh"
@@ -7,23 +12,32 @@ class CgumhSpider(scrapy.Spider):
 
     start_urls = [
         "https://www.cgmh.org.tw/tw/Systems/RecruitInfo/3?bulletinType=A&category=Z"
-        
         ]
 
     def parse(self, response):
+        # <processing code not shown>
+        item = HospworkItem()
+        # populate `item` fields
+        # and extract item_details_url
         url_query = dict(parse_qsl(urlparse(response.request.url).query))
 
         for job in response.xpath("//div[@id='list']/div/ul[@class='ul-reset']/li"):
-            NAME_SELECTOR = 'div.fz-20.block ::text'
+            #NAME_SELECTOR = 'div.fz-20.block ::text'
             RRP_SELECTOR = 'a ::attr(href)'
-            job_title = job.css(NAME_SELECTOR).get()
-            yield {
-                'name': job_title,
-                'job_link': response.urljoin(job.css(RRP_SELECTOR).extract_first()),
-                'hosp_region': self.get_hosp_region(job_title),
-            }
+            #job_title = job.css(NAME_SELECTOR).get()
+            job_link = response.urljoin(job.css(RRP_SELECTOR).extract_first())
+            #hosp_region = self.get_hosp_region(job_title)
+            item['data_source'] = 'cgmh'
+            item['hosp_name'] = '長庚醫院'
+            #item['job_name'] = job_title
+            #item['job_link'] = job_link
+            #item['hosp_region'] = hosp_region
 
-        next_page = response.xpath("//ul[@class='layout__pagination ul-reset']/li/a/@href").getall()[-1].replace("#list",'')
+            yield scrapy.Request(
+                job_link, self.parse_details, meta={"item": item}
+            )
+
+        #next_page = response.xpath("//ul[@class='layout__pagination ul-reset']/li/a/@href").getall()[-1].replace("#list",'')
 
         if next_page and next_page != "javascript:void(0)":
             next_page_urlparse = urlparse(next_page)
@@ -33,7 +47,43 @@ class CgumhSpider(scrapy.Spider):
                 response.urljoin(next_page_urlparse._replace(query=urlencode(url_query)).geturl()),
                 callback=self.parse
             )
-    
+
+    def parse_details(self, response):
+        item = response.meta["item"]
+        title = clean_unused_str(response.xpath("//head/title/text()").get(),item['hosp_name']).replace('|長庚醫療財團法人全球資訊網','')
+        item['hosp_region'] = self.get_hosp_region(title)
+        title = title.replace('院','').replace("紀念",'').replace("新北市立","").replace("高雄市立鳳山","").replace("長庚","")
+        title = title.replace(item['hosp_region'][:-2],'')
+        title_ = title
+        title_old = title
+
+        work_detail_web = clean_unused_str(''.join(response.css("article.fz-17::text").getall()), title)
+
+        dead_line = self.get_work_dead_line( work_detail_web ,title)
+
+        if '工作地點' in work_detail_web:
+            originzation = findjoboriginzation(work_detail_web.rsplit('工作地點')[1], item['hosp_name'] )
+        elif (originzation := findjoboriginzation((title_ if (title_ :=  title_old) else title), item['hosp_name'] )) and originzation != title:
+            originzation = originzation
+            title = title.replace(originzation,'')
+        elif (originzation := findjoboriginzation(work_detail_web, item['hosp_name']) ) and type(originzation) == str:
+            originzation = originzation
+            title = title.replace(originzation,'')
+        else:
+            print("Error find originaztion",response.url)
+            originzation = ''
+
+        if (new_title :=  findjobtype(title, self.name)) and new_title != title:
+            title = new_title.replace('醫院','').replace("紀念",'').replace("新北市立","").replace("高雄市立鳳山","")
+
+        item['job_name'] = title
+        item['job_link'] = response.request.url
+        item['job_deadline'] = dead_line
+        item['job_originzation'] = originzation
+
+        yield item
+
+   
     def get_hosp_region(self,title):
         '''
         get hospital region for cgumh
@@ -60,3 +110,44 @@ class CgumhSpider(scrapy.Spider):
             return '鳳山院區'
         elif '北院區' in title:
             return '北院區'
+
+    def get_work_dead_line(self, work_detail_web, title):
+        work_detail_web = work_detail_web.replace('(日) ','').replace('(日)','').replace('（含）','').replace('即日起，至','即日起至')
+        if '報名期限展延至' in work_detail_web:
+            return re.findall("\d+年\d+月\d+日",work_detail_web.rsplit('報名期限展延至')[1].split("止")[0].replace(' ',''))[0]
+        elif '額滿' in work_detail_web:
+            return '額滿為止'
+        elif '招募合適人選為止' in work_detail_web or '招募到合適⼈選為' in work_detail_web or '徵到為止' in work_detail_web:
+            return '招募合適人選為止'
+        elif '隨到隨審' in work_detail_web:
+            return '隨到隨審'
+        elif '即日起至招聘完成' in work_detail_web:
+            return '即日起至招聘完成'
+        elif '自即日起' in work_detail_web or '即日起~' in work_detail_web:
+            return '自即日起'
+        elif '即日起至' in work_detail_web:
+            if (new_work_detail_web := work_detail_web.rsplit('即日起至')[1]) and "止" in new_work_detail_web:
+                if '截止' in new_work_detail_web:
+                    return clean_date(new_work_detail_web.split("截止")[0].replace(' ',''), self.name)
+                else:
+                    return clean_date(new_work_detail_web.split("止")[0].replace(' ',''), self.name)
+            elif (new_work_detail_web := work_detail_web.rsplit('即日起至')[1]) and  "前" in work_detail_web:
+                return clean_date(new_work_detail_web.split("前")[0].replace(' ',''), self.name)
+            elif "/" in work_detail_web.rsplit('即日起至')[1]:
+                return re.findall("\d+/\d+/\d+",work_detail_web.rsplit('即日起至')[1].split("。")[0].replace(' ',''))[0]
+        elif '即日起收件至' in work_detail_web:
+            return work_detail_web.rsplit("即日起收件至")[1].split("止")[0].replace("：","")
+        elif '前報名完成' in work_detail_web:
+            return work_detail_web.rsplit("請於")[1].split("前")[0].replace("：","")
+        else:
+            try:
+                if '截止' in work_detail_web.rsplit("報名期限")[1]:
+                    return work_detail_web.rsplit("報名期限")[1].split("截止")[0].replace("：","")
+                else:
+                    return work_detail_web.rsplit("報名期限")[1].split("。")[0].replace("：","")
+            except:
+                try:
+                    return re.findall("\d+年\d+月\d+日",work_detail_web)[0]
+                except:
+                    print(self.name, title)
+                    return 'please check webpage'
