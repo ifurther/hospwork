1d6a2860a3d2987b64fe1faf82a7e117a6e65242
ifurther <55025025+ifurther@users.noreply.github.com>
2025-02-16 23:39:23 +0800

    scarpy.item: update originazition name

--- hospwork/hospwork/items.py
+++ hospwork/hospwork/items.py
@@ -4,9 +4,14 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
+from dataclasses import dataclass
 
 class HospworkItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    data_source = scrapy.Field()
+    hosp_name = scrapy.Field()
+    job_name = scrapy.Field()
+    job_link = scrapy.Field()
+    job_deadline = scrapy.Field()
+    hosp_region = scrapy.Field()
+    job_originzation = scrapy.Field()
