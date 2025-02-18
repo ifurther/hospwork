bce1044f2102800ef88bcc84b82473776b08e46a
furtherW <ifurther2233@outlook.com>
2025-02-18 23:01:35 +0800

    scrapy.vghtc: update parse function for next page
    Signed-off-by: furtherW <ifurther2233@outlook.com>
    Signed-off-by: ifurther <55025025+ifurther@users.noreply.github.com>

--- hospwork/hospwork/spiders/vghtc.py
+++ hospwork/hospwork/spiders/vghtc.py
@@ -1,10 +1,28 @@
 import scrapy
+from urllib.parse import urlparse,urlencode,parse_qsl
 
+from hospwork.items import HospworkItem
+
+from hospwork.tool.web import get_base_web_data,get_work_page
+from hospwork.tool.job import findjoboriginzation,findjobtype,clean_unused_str
+from hospwork.tool.time import clean_date
 
 class VghtcSpider(scrapy.Spider):
     name = "vghtc"
     allowed_domains = ["www.vghtc.gov.tw"]
-    start_urls = ["https://www.vghtc.gov.tw"]
+    start_urls = ["https://www.vghtc.gov.tw/Module/RecruitMent?WebMenuID=9d005e46-411b-46fc-b438-e0bd561eba78&page=2"]
 
     def parse(self, response):
-        pass
+        item = HospworkItem()
+
+        url_query = dict(parse_qsl(urlparse(response.request.url).query))
+
+        next_page = response.xpath("//*[@id="content"]/div[@id="pager"]/@href").getall()[-1].replace("#list",'')
+        if next_page and next_page != "javascript:void(0)":
+            next_page_urlparse = urlparse(next_page)
+            next_query = dict(parse_qsl(next_page_urlparse.query))
+            url_query.update(next_query)
+            yield scrapy.Request(
+                response.urljoin(next_page_urlparse._replace(query=urlencode(url_query)).geturl()),
+                callback=self.parse
+            )   
